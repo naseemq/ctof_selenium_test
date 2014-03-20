03c7f9c78d51ce9ede83ca780ebc13fe35789b95
naseemq <naseem_qureshi@intuit.com>
2014-03-20 15:46:31 -0700

    added new singin test cases

--- spec/browser/home_spec.rb
+++ spec/browser/home_spec.rb
@@ -25,10 +25,59 @@ describe 'Home' do
     end
 
     it 'should sign-in and register ban user', :browser, :suite => 'regression' do
-      sign_in_mtv_auth_user
+      sign_in_ban_auth_user
 
       @register_modal.modal_title.should include 'Register for CTOF 2014'
       @register_modal.welcome_user.should == 'Welcome Santosh!'
+    end
+
+    it 'should sign-in and register bur user', :browser, :suite => 'regression' do
+      sign_in_bur_auth_user
+
+      @register_modal.modal_title.should include 'Register for CTOF 2014'
+      @register_modal.welcome_user.should == 'Welcome Xinli!'
+    end
+
+    it 'should sign-in and register cmb user', :browser, :suite => 'regression' do
+      sign_in_cmb_auth_user
+
+      @register_modal.modal_title.should include 'Register for CTOF 2014'
+      @register_modal.welcome_user.should == 'Welcome Christine!'
+    end
+
+    it 'should sign-in and register edm user', :browser, :suite => 'regression' do
+      sign_in_edm_auth_user
+
+      @register_modal.modal_title.should include 'Register for CTOF 2014'
+      @register_modal.welcome_user.should == 'Welcome Bradley!'
+    end
+
+    it 'should sign-in and register mis user', :browser, :suite => 'regression' do
+      sign_in_mis_auth_user
+
+      @register_modal.modal_title.should include 'Register for CTOF 2014'
+      @register_modal.welcome_user.should == 'Welcome Graham!'
+    end
+
+    it 'should sign-in and register pln user', :browser, :suite => 'regression' do
+      sign_in_pln_auth_user
+
+      @register_modal.modal_title.should include 'Register for CTOF 2014'
+      @register_modal.welcome_user.should == 'Welcome Mary!'
+    end
+
+    it 'should sign-in and register rmt user', :browser, :suite => 'regression' do
+      sign_in_rmt_auth_user
+
+      @register_modal.modal_title.should include 'Register for CTOF 2014'
+      @register_modal.welcome_user.should == 'Welcome Alexander!'
+    end
+
+    it 'should sign-in and register wdh user', :browser, :suite => 'regression' do
+      sign_in_wdh_auth_user
+
+      @register_modal.modal_title.should include 'Register for CTOF 2014'
+      @register_modal.welcome_user.should == 'Welcome Tori!'
     end
 
   end
