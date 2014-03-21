af65f953f24fbcd4cbc292fb9b400020a53e35c7
skam1 <stephen.h.kam@gmail.com>
2014-03-20 18:32:06 -0700

    Added select attendance from dropdown menu for user registration

--- spec/browser/home_spec.rb
+++ spec/browser/home_spec.rb
@@ -10,74 +10,111 @@ describe 'Home' do
 
   describe 'Sign-In & Register' do
 
-    it 'should sign-in and register sdg user', :browser, :suite => 'regression' do
+    it 'should sign-in and register sdg user', :browser, :suite => 'smoke' do
       sign_in_sdg_auth_user
 
       @register_modal.modal_title.should include 'Register for CTOF 2014'
       @register_modal.welcome_user.should == 'Welcome Jonathan!'
+      @register_modal.location_label.should == 'San Diego'
     end
 
-    it 'should sign-in and register mtv user', :browser, :suite => 'regression' do
+    it 'should sign-in and register mtv user', :browser, :suite => 'smoke' do
       sign_in_mtv_auth_user
 
       @register_modal.modal_title.should include 'Register for CTOF 2014'
       @register_modal.welcome_user.should == 'Welcome Bhushan!'
+      @register_modal.location_label.should == 'Mountain View'
     end
 
-    it 'should sign-in and register ban user', :browser, :suite => 'regression' do
+    it 'should sign-in and register ban user', :browser, :suite => 'smoke' do
       sign_in_ban_auth_user
 
       @register_modal.modal_title.should include 'Register for CTOF 2014'
       @register_modal.welcome_user.should == 'Welcome Santosh!'
+      @register_modal.location_label.should == 'Bangalore'
     end
 
-    it 'should sign-in and register bur user', :browser, :suite => 'regression' do
+    it 'should sign-in and register bur user', :browser, :suite => 'smoke' do
       sign_in_bur_auth_user
 
       @register_modal.modal_title.should include 'Register for CTOF 2014'
       @register_modal.welcome_user.should == 'Welcome Xinli!'
+      @register_modal.location_label.should == 'Burlingame'
     end
 
-    it 'should sign-in and register cmb user', :browser, :suite => 'regression' do
+    it 'should sign-in and register cmb user', :browser, :suite => 'smoke' do
       sign_in_cmb_auth_user
 
       @register_modal.modal_title.should include 'Register for CTOF 2014'
       @register_modal.welcome_user.should == 'Welcome Ashish!'
+      @register_modal.location_label.should == 'Cambridge'
     end
 
-    it 'should sign-in and register edm user', :browser, :suite => 'regression' do
+    it 'should sign-in and register edm user', :browser, :suite => 'smoke' do
       sign_in_edm_auth_user
 
       @register_modal.modal_title.should include 'Register for CTOF 2014'
       @register_modal.welcome_user.should == 'Welcome Bradley!'
+      @register_modal.location_label.should == 'Edmonton'
     end
 
-    it 'should sign-in and register mis user', :browser, :suite => 'regression' do
+    it 'should sign-in and register mis user', :browser, :suite => 'smoke' do
       sign_in_mis_auth_user
 
       @register_modal.modal_title.should include 'Register for CTOF 2014'
       @register_modal.welcome_user.should == 'Welcome Graham!'
+      @register_modal.location_label.should == 'Mississauga'
     end
 
-    it 'should sign-in and register pln user', :browser, :suite => 'regression' do
+    it 'should sign-in and register pln user', :browser, :suite => 'smoke' do
       sign_in_pln_auth_user
 
       @register_modal.modal_title.should include 'Register for CTOF 2014'
       @register_modal.welcome_user.should == 'Welcome Mary!'
+      @register_modal.location_label.should == 'Plano'
     end
 
-    it 'should sign-in and register rmt user', :browser, :suite => 'regression' do
+    it 'should sign-in and register rmt user', :browser, :suite => 'smoke' do
       sign_in_rmt_auth_user
 
       @register_modal.modal_title.should include 'Register for CTOF 2014'
       @register_modal.welcome_user.should == 'Welcome Alexander!'
+      @register_modal.location_label.should == 'Remote'
     end
 
-    it 'should sign-in and register wdh user', :browser, :suite => 'regression' do
+    it 'should sign-in and register wdh user', :browser, :suite => 'smoke' do
       sign_in_wdh_auth_user
 
       @register_modal.modal_title.should include 'Register for CTOF 2014'
       @register_modal.welcome_user.should == 'Welcome Tori!'
+      @register_modal.location_label.should == 'Woodland Hills'
+    end
+  end
+
+  describe 'Register user' do
+
+    it 'should select attendance:yes for sdg user', :browser, :suite => 'smoke' do
+      sign_in_sdg_auth_user
+
+      @register_modal.select_attendance('Yes!')
+      sleep 3 #to see what option selected - need to remove later
+      @register_modal.click_submit
+    end
+
+    it 'should select attendance:sorry for sdg user', :browser, :suite => 'smoke' do
+      sign_in_sdg_auth_user
+
+      @register_modal.select_attendance('Sorry, can\'t make it')
+      sleep 3 #to see what option selected - need to remove later
+      @register_modal.click_submit
+    end
+
+    it 'should select attendance:tuesday only for sdg user', :browser, :suite => 'smoke' do
+      sign_in_sdg_auth_user
+
+      @register_modal.select_attendance('I can only go on Tuesday')
+      sleep 3 #to see what option selected - need to remove later
+      @register_modal.click_submit
     end
 
   end
--- spec/browser/support/register_modal.rb
+++ spec/browser/support/register_modal.rb
@@ -11,8 +11,29 @@ module PageObjects
       @driver.find_element(:css, 'h3.modal-title').text
     end
 
+    def location_label
+      @driver.find_element(:css, 'h4 span.text-muted.ng-binding').text
+    end
+
     def welcome_user
       @driver.find_element(:css, 'strong.ng-binding').text
+    end
+
+    def select_attendance(response)
+      @driver.find_elements(:css, 'select.form-control').each_with_index do |menu, index|
+        #first dropdown menu
+        if index == 0
+          menu.find_elements(:tag_name, 'option').each do |choice|
+            if choice.text.eql?(response)
+              choice.click
+            end
+          end
+        end
+      end
+    end
+
+    def click_submit
+      @driver.find_element(:css, 'button.btn.btn-primary').click
     end
 
   end
