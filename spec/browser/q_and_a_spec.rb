ec1dade16c73695bf84bfa099e27b2fb983c9198
skam1 <stephen.h.kam@gmail.com>
2014-05-02 11:21:53 -0700

    Added Session Q&A page: Comment on a Question test cases

--- spec/browser/q_and_a_spec.rb
+++ spec/browser/q_and_a_spec.rb
@@ -5,6 +5,7 @@ include BrowserSpecUtils
 include ActionView::Helpers::TextHelper
 
 TestQuestionText = "Test Session Q&A question"
+TestCommentText = "QA test comment"
 
 describe 'Session Q&A' do
 
@@ -12,7 +13,8 @@ describe 'Session Q&A' do
 
   describe 'Session Q&A page' do
 
-    it 'sdg user should submit a question', :browser, :suite => 'smoke' do
+    #Ask a question
+    it 'should allow sdg user to submit a question', :browser, :suite => 'smoke' do
       sign_in_sdg_auth_user
       @register_modal.register_sdg_user
       @dashboard.click_session("Fireside chat w/ Box")
@@ -28,7 +30,7 @@ describe 'Session Q&A' do
       @home.reset_user
     end
 
-    it 'mtv user should submit a question', :browser, :suite => 'regression' do
+    it 'should allow mtv user to submit a question', :browser, :suite => 'regression' do
       sign_in_mtv_auth_user
       @register_modal.register_mtv_user
       @dashboard.click_session("Fireside chat w/ Box")
@@ -44,7 +46,7 @@ describe 'Session Q&A' do
       @home.reset_user
     end
 
-    it 'ban user should submit a question', :browser, :suite => 'regression' do
+    it 'should allow ban user to submit a question', :browser, :suite => 'regression' do
       sign_in_ban_auth_user
       @register_modal.register_ban_user
       @dashboard.click_session("IDC Keynote")
@@ -60,7 +62,7 @@ describe 'Session Q&A' do
       @home.reset_user
     end
 
-    it 'bur user should submit a question', :browser, :suite => 'regression' do
+    it 'should allow bur user to submit a question', :browser, :suite => 'regression' do
       sign_in_bur_auth_user
       @register_modal.register_bur_user
       @dashboard.click_session("Fireside chat w/ Box")
@@ -76,7 +78,7 @@ describe 'Session Q&A' do
       @home.reset_user
     end
 
-    it 'cmb user should submit a question', :browser, :suite => 'regression' do
+    it 'should allow cmb user to submit a question', :browser, :suite => 'regression' do
       sign_in_cmb_auth_user
       @register_modal.register_cmb_user
       @dashboard.click_session("Fireside chat w/ Box")
@@ -92,7 +94,7 @@ describe 'Session Q&A' do
       @home.reset_user
     end
 
-    it 'edm user should submit a question', :browser, :suite => 'regression' do
+    it 'should allow edm user to submit a question', :browser, :suite => 'regression' do
       sign_in_edm_auth_user
       @register_modal.register_edm_user
       @dashboard.click_session("Fireside chat w/ Box")
@@ -108,7 +110,7 @@ describe 'Session Q&A' do
       @home.reset_user
     end
 
-    it 'mis user should submit a question', :browser, :suite => 'regression' do
+    it 'should allow mis user to submit a question', :browser, :suite => 'regression' do
       sign_in_mis_auth_user
       @register_modal.register_mis_user
       @dashboard.click_session("Fireside chat w/ Box")
@@ -124,7 +126,7 @@ describe 'Session Q&A' do
       @home.reset_user
     end
 
-    it 'pln user should submit a question', :browser, :suite => 'test' do
+    it 'should allow pln user to submit a question', :browser, :suite => 'regression' do
       sign_in_pln_auth_user
       @register_modal.register_pln_user
       @dashboard.click_session("Fireside chat w/ Box")
@@ -140,7 +142,7 @@ describe 'Session Q&A' do
       @home.reset_user
     end
 
-    it 'rmt user should submit a question', :browser, :suite => 'regression' do
+    it 'should allow rmt user to submit a question', :browser, :suite => 'regression' do
       sign_in_rmt_auth_user
       @register_modal.register_rmt_user
       @dashboard.click_session("Fireside chat w/ Box")
@@ -156,7 +158,7 @@ describe 'Session Q&A' do
       @home.reset_user
     end
 
-    it 'wdh user should submit a question', :browser, :suite => 'regression' do
+    it 'should allow wdh user to submit a question', :browser, :suite => 'regression' do
       sign_in_wdh_auth_user
       @register_modal.register_wdh_user
       @dashboard.click_session("Fireside chat w/ Box")
@@ -165,6 +167,208 @@ describe 'Session Q&A' do
       @q_and_a_page.question_entry_box=TestQuestionText
       @q_and_a_page.click_submit
       @q_and_a_page.question_text.should include(TestQuestionText)
+
+      @q_and_a_page.click_delete_your_question
+      @q_and_a_page.close_alert
+
+      @home.reset_user
+    end
+
+
+    #Comment on a question
+    it 'should allow sdg user to comment a question', :browser, :suite => 'smoke' do
+      sign_in_sdg_auth_user
+      @register_modal.register_sdg_user
+      @dashboard.click_session("Fireside chat w/ Box")
+      @session_details.click_session_q_and_a
+
+      @q_and_a_page.question_entry_box=TestQuestionText
+      @q_and_a_page.click_submit
+
+      @q_and_a_page.click_comment
+      @q_and_a_page.comment_entry_box=TestCommentText
+      @q_and_a_page.click_submit_comment
+      @q_and_a_page.comment_text.should include(TestCommentText)
+
+      @q_and_a_page.click_delete_your_question
+      @q_and_a_page.close_alert
+
+      @home.reset_user
+    end
+
+    it 'should allow mtv user to comment a question', :browser, :suite => 'regression' do
+      sign_in_mtv_auth_user
+      @register_modal.register_mtv_user
+      @dashboard.click_session("Fireside chat w/ Box")
+      @session_details.click_session_q_and_a
+
+      @q_and_a_page.question_entry_box=TestQuestionText
+      @q_and_a_page.click_submit
+
+      @q_and_a_page.click_comment
+      @q_and_a_page.comment_entry_box=TestCommentText
+      @q_and_a_page.click_submit_comment
+      @q_and_a_page.comment_text.should include(TestCommentText)
+
+      @q_and_a_page.click_delete_your_question
+      @q_and_a_page.close_alert
+
+      @home.reset_user
+    end
+
+    it 'should allow ban user to comment a question', :browser, :suite => 'regression' do
+      sign_in_ban_auth_user
+      @register_modal.register_ban_user
+      @dashboard.click_session("IDC Keynote")
+      @session_details.click_session_q_and_a
+
+      @q_and_a_page.question_entry_box=TestQuestionText
+      @q_and_a_page.click_submit
+
+      @q_and_a_page.click_comment
+      @q_and_a_page.comment_entry_box=TestCommentText
+      @q_and_a_page.click_submit_comment
+      @q_and_a_page.comment_text.should include(TestCommentText)
+
+      @q_and_a_page.click_delete_your_question
+      @q_and_a_page.close_alert
+
+      @home.reset_user
+    end
+
+    it 'should allow bur user to comment a question', :browser, :suite => 'regression' do
+      sign_in_bur_auth_user
+      @register_modal.register_bur_user
+      @dashboard.click_session("Fireside chat w/ Box")
+      @session_details.click_session_q_and_a
+
+      @q_and_a_page.question_entry_box=TestQuestionText
+      @q_and_a_page.click_submit
+
+      @q_and_a_page.click_comment
+      @q_and_a_page.comment_entry_box=TestCommentText
+      @q_and_a_page.click_submit_comment
+      @q_and_a_page.comment_text.should include(TestCommentText)
+
+      @q_and_a_page.click_delete_your_question
+      @q_and_a_page.close_alert
+
+      @home.reset_user
+    end
+
+    it 'should allow cmb user to comment a question', :browser, :suite => 'regression' do
+      sign_in_cmb_auth_user
+      @register_modal.register_cmb_user
+      @dashboard.click_session("Fireside chat w/ Box")
+      @session_details.click_session_q_and_a
+
+      @q_and_a_page.question_entry_box=TestQuestionText
+      @q_and_a_page.click_submit
+
+      @q_and_a_page.click_comment
+      @q_and_a_page.comment_entry_box=TestCommentText
+      @q_and_a_page.click_submit_comment
+      @q_and_a_page.comment_text.should include(TestCommentText)
+
+      @q_and_a_page.click_delete_your_question
+      @q_and_a_page.close_alert
+
+      @home.reset_user
+    end
+
+    it 'should allow edm user to comment a question', :browser, :suite => 'regression' do
+      sign_in_edm_auth_user
+      @register_modal.register_edm_user
+      @dashboard.click_session("Fireside chat w/ Box")
+      @session_details.click_session_q_and_a
+
+      @q_and_a_page.question_entry_box=TestQuestionText
+      @q_and_a_page.click_submit
+
+      @q_and_a_page.click_comment
+      @q_and_a_page.comment_entry_box=TestCommentText
+      @q_and_a_page.click_submit_comment
+      @q_and_a_page.comment_text.should include(TestCommentText)
+
+      @q_and_a_page.click_delete_your_question
+      @q_and_a_page.close_alert
+
+      @home.reset_user
+    end
+
+    it 'should allow mis user to comment a question', :browser, :suite => 'regression' do
+      sign_in_mis_auth_user
+      @register_modal.register_mis_user
+      @dashboard.click_session("Fireside chat w/ Box")
+      @session_details.click_session_q_and_a
+
+      @q_and_a_page.question_entry_box=TestQuestionText
+      @q_and_a_page.click_submit
+
+      @q_and_a_page.click_comment
+      @q_and_a_page.comment_entry_box=TestCommentText
+      @q_and_a_page.click_submit_comment
+      @q_and_a_page.comment_text.should include(TestCommentText)
+
+      @q_and_a_page.click_delete_your_question
+      @q_and_a_page.close_alert
+
+      @home.reset_user
+    end
+
+    it 'should allow pln user to comment a question', :browser, :suite => 'regression' do
+      sign_in_pln_auth_user
+      @register_modal.register_pln_user
+      @dashboard.click_session("Fireside chat w/ Box")
+      @session_details.click_session_q_and_a
+
+      @q_and_a_page.question_entry_box=TestQuestionText
+      @q_and_a_page.click_submit
+
+      @q_and_a_page.click_comment
+      @q_and_a_page.comment_entry_box=TestCommentText
+      @q_and_a_page.click_submit_comment
+      @q_and_a_page.comment_text.should include(TestCommentText)
+
+      @q_and_a_page.click_delete_your_question
+      @q_and_a_page.close_alert
+
+      @home.reset_user
+    end
+
+    it 'should allow rmt user to comment a question', :browser, :suite => 'regression' do
+      sign_in_rmt_auth_user
+      @register_modal.register_rmt_user
+      @dashboard.click_session("Fireside chat w/ Box")
+      @session_details.click_session_q_and_a
+
+      @q_and_a_page.question_entry_box=TestQuestionText
+      @q_and_a_page.click_submit
+
+      @q_and_a_page.click_comment
+      @q_and_a_page.comment_entry_box=TestCommentText
+      @q_and_a_page.click_submit_comment
+      @q_and_a_page.comment_text.should include(TestCommentText)
+
+      @q_and_a_page.click_delete_your_question
+      @q_and_a_page.close_alert
+
+      @home.reset_user
+    end
+
+    it 'should allow wdh user to comment a question', :browser, :suite => 'regression' do
+      sign_in_wdh_auth_user
+      @register_modal.register_wdh_user
+      @dashboard.click_session("Fireside chat w/ Box")
+      @session_details.click_session_q_and_a
+
+      @q_and_a_page.question_entry_box=TestQuestionText
+      @q_and_a_page.click_submit
+
+      @q_and_a_page.click_comment
+      @q_and_a_page.comment_entry_box=TestCommentText
+      @q_and_a_page.click_submit_comment
+      @q_and_a_page.comment_text.should include(TestCommentText)
 
       @q_and_a_page.click_delete_your_question
       @q_and_a_page.close_alert
--- spec/browser/support/q_and_a_page.rb
+++ spec/browser/support/q_and_a_page.rb
@@ -6,9 +6,20 @@ module PageObjects
       @driver = driver
     end
 
-    def close_alert
-      alert = @driver.switch_to.alert
-      alert.accept
+    def question_text
+      @driver.find_element(:css, 'div.media-body.ng-binding').text
+    end
+
+    def comment_text
+      comment = ""
+      @driver.find_elements(:css, 'div.comment.ng-scope small.ng-binding').each_with_index do |line_of_comment_text, index|
+        #need to get the SECOND line of comment text to validate (first line says who commented and from what location)
+        if index == 1
+          comment = line_of_comment_text.text
+        end
+      end
+      #return comment
+      comment
     end
 
     def question_entry_box=(question)
@@ -16,8 +27,9 @@ module PageObjects
       element.send_keys question
     end
 
-    def question_text
-      @driver.find_element(:css, 'div.media-body.ng-binding').text
+    def comment_entry_box=(comment)
+      element = @driver.find_element(:css, 'div.form-group textarea.form-control.ng-pristine.ng-valid')
+      element.send_keys comment
     end
 
     def click_submit
@@ -25,9 +37,23 @@ module PageObjects
       sleep 2
     end
 
+    def click_submit_comment
+      @driver.find_element(:css, 'div.question.panel.panel-default.ng-scope button.btn.btn-primary').click
+      sleep 2
+    end
+
+    def click_comment
+      @driver.find_element(:css, 'i.icon-comment').click
+    end
+
     def click_delete_your_question
       @driver.find_element(:css, 'button.btn.btn-small.btn-danger').click
     end
+
+    def close_alert
+    alert = @driver.switch_to.alert
+    alert.accept
+  end
 
   end
 end
